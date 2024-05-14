85ebf36617b82e6da1139d54332eea6de9a73aa8
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-14 16:22:53 +0100

    creating menu model

--- models/Menu.js
+++ models/Menu.js
@@ -1,6 +1,17 @@
 const {sequelize} = require('../db');
-const { Sequelize } = require('sequelize');
+const { Sequelize, Model, DataTypes } = require('sequelize');
 
 // TODO - create a Menu model
+
+class Menu extends Model {}
+
+Menu.init(
+    {
+        title: DataTypes.STRING
+    }, {
+        sequelize: sequelize,
+        modelName: "Menu"
+    }
+)
 
 module.exports = {Menu};
