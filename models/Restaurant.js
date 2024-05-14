58cfa68aee868b6593ef2972cdbe8434d0197fd4
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-14 16:21:47 +0100

    creating restaurant model

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,6 +1,19 @@
+const { DESCRIBE } = require('sequelize/lib/query-types');
 const {sequelize} = require('../db');
-const { Sequelize } = require('sequelize');
+const { Sequelize, Model, DataTypes } = require('sequelize');
 
 // TODO - create a Restaurant model
+class Restaurant extends Model {}
+
+Restaurant.init(
+    {
+        name: DataTypes.STRING,
+        location: DataTypes.STRING,
+        cuisine: DataTypes.STRING
+    }, {
+        sequelize: sequelize,
+        modelName: "Restaurant"
+    }
+)
 
 module.exports = {Restaurant};
